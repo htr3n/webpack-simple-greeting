9eec6654cbc07d7e577a110c91c163386e41dde8
Huy Tran <hoang.huy.tran@gmail.com>
2018-02-18 12:47:23 +1100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  }
+};
